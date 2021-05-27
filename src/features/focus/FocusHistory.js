c50b2dabbab9a2cb7e7cfb20252565bddfc14d59
apufouzder <apufouzdar@gmail.com>
2021-05-27 21:01:26 +0600

    my first app

---
+++ src/features/focus/FocusHistory.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { fontSizes, spacing } from '../../utlis/sizes';
+import { RoundedButton } from '../../components/RoundedButton';
+
+const HistoryItem = ({ item, index }) => {
+  return <Text style={styles.historyItem(item.status)}>{item.subject}</Text>;
+};
+
+export const FocusHistory = ({ focusHistory, onClear }) => {
+  const clearHistory = () => {
+    onClear();
+  };
+
+  return (
+    <>
+      <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+        {!!focusHistory.length && (
+          <>
+            <Text style={styles.title}> Thing we've focusing on </Text>
+            <FlatList
+              style={{ flex: 0.5 }}
+              contentContainerStyle={{ flex: 1, alignItems: 'center' }}
+              data={focusHistory}
+              renderItem={HistoryItem}
+            />
+            <View style={styles.clearContainer}>
+              <RoundedButton
+                size={75}
+                title="Clear"
+                onPress={() => onClear()}
+              />
+            </View>
+          </>
+        )}
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  historyItem: (status) => ({
+    color: status > 1 ? 'red' : 'green',
+    fontSize: fontSizes.md,
+  }),
+  title: {
+    color: 'white',
+    fontSize: fontSizes.lg,
+  },
+  clearContainer: {
+    alignItems: 'center',
+    padding: spacing.md,
+  },
+});
